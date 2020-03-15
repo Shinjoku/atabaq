143f7c847aa659e96715cc9858614662db56f347
Shinjoku <deora.2@hotmail.com>
2020-03-15 04:38:51 -0300

    feat: Implements Grid component

---
+++ src/IconGrid.vue
@@ -0,0 +1,26 @@
+<template>
+        <div>
+            <grid-item 
+                v-for="(gridItem, i) in itemsList"
+                :key="i"
+                :index="i"
+            />
+        </div>
+</template>
+
+<script>
+export default {
+    props: {
+        width: Number,
+        height: Number
+    },
+    data() {
+        let iconCount = ((this.width * this.height) - itemArea) / itemSize ** 2;
+
+        return {
+            grid: iconList,
+            itemsList: getItems()
+        }
+    }
+}
+</script>
